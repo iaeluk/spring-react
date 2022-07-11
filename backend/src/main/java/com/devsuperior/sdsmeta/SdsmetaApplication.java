301e410ba4b42600041691cd3886fb5e3b90aace
Lucas Mendonça <lucas21rio@gmail.com>
2022-07-11 15:18:07 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/sdsmeta/SdsmetaApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.sdsmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdsmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdsmetaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/sdsmeta/SdsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.sdsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
